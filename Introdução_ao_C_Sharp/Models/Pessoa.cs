b75202c7f1f743761c2d48eb86945e11529dc07c
Adriano Lengruber <85639595+Adriano-Lengruber@users.noreply.github.com>
2023-03-24 12:07:35 -0300

    arrumando pastas

---
+++ Introdução_ao_C_Sharp/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Introdução_ao_C_Sharp.Models
+{
+    public class Pessoa
+    {
+        
+    }
+}
